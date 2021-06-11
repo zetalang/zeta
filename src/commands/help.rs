c763c2c7fdbeaa02ebb7a93136451497a9285182
Varun Potti <potti.varun07@gmail.com>
2021-06-11 13:25:33 +0530

    fix: change name from bolt to torq

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -17,58 +17,24 @@ pub struct Help;
 
 #[async_trait]
 impl Command for Help {
-    /// Display a help menu for the `volt help` command.
     fn help() -> String {
         format!(
-            r#"volt {}
+            r#"torqc {}
     
 Displays help information.
 Usage: {} {} {}
 Commands:
-  {} {} - Install all dependencies for a project.
-  {} {} - Interactively create or update a package.json file for a project.
-  {} {} - Add a dependency to a project.
-  {} {} - Remove a dependency from the package.json file for a project.
-  {} {} - Push changes to a github repository the easy way.
-  {} {} - Clean the volt cache files and metadata.
-  {} {} - Clone a github repository and get setup with all required dependencies.
+  {} {} - Compiles the given file 
   "#,
             VERSION.bright_green().bold(),
-            "volt".bright_green().bold(),
+            "torqc".bright_green().bold(),
             "[commands]".bright_purple(),
             "[flags]".bright_purple(),
             "*".bright_magenta().bold(),
-            "install".bright_blue(),
-            "*".bright_magenta().bold(),
-            "init".bright_blue(),
-            "*".bright_magenta().bold(),
-            "add".bright_blue(),
-            "*".bright_magenta().bold(),
-            "remove".bright_blue(),
-            "*".bright_magenta().bold(),
-            "cache".bright_blue(),
-            "*".bright_magenta().bold(),
-            "deploy".bright_blue(),
-            "*".bright_magenta().bold(),
-            "clone".bright_blue(),
+            "<filename>".bright_blue(),
         )
     }
 
-    /// Execute the `volt help` command
-    ///
-    /// Displays help information.
-    /// ## Arguments
-    /// * `app` - Instance of the command (`Arc<App>`)
-    /// * `packages` - List of packages to add (`Vec<String>`)
-    /// * `flags` - List of flags passed in through the CLI (`Vec<String>`)
-    /// ## Examples
-    /// ```
-    /// // Display a help menu.
-    /// // .exec() is an async call so you need to await it
-    /// Help.exec(app, vec![], vec![]).await;
-    /// ```
-    /// ## Returns
-    /// * `Result<()>`
     async fn exec(_app: Arc<App>) -> Result<()> {
         println!("{}", Self::help());
         Ok(())
