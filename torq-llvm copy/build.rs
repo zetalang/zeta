dc0956a5d52f7376d22fc5104768f7eeee7c60d2
VarunPotti <potti.varun07@gmail.com>
2021-06-21 21:12:49 +0530

    trying to fix submodule

--- torq-llvm copy/build.rs
+++
@@ -1,7 +0,0 @@
-extern crate cmake;
-
-fn main() {
-    let dst = cmake::build("wrappers");
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=llvm-wrappers");
-}
